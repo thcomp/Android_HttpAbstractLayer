b52cb945978373214f2032f14c81fde063bc558d
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2018-04-04 16:33:28 +0900

    Xamarin向けのBinding Library追加

---
+++ XamarinBinding/UnitTestApp/TestSample.cs
@@ -0,0 +1,136 @@
+﻿using NUnit.Framework;
+using ThcompHAL = JP.CO.Thcomp.Http_abstract_layer;
+
+
+namespace UnitTestApp
+{
+    [TestFixture]
+    public class TestsSample
+    {
+
+        [SetUp]
+        public void Setup() { }
+
+
+        [TearDown]
+        public void Tear() { }
+
+        [Test]
+        public void TestHttpAccessLayer1()
+        {
+            ThcompHAL.HttpAccessLayer accessLayer = ThcompHAL.HttpAccessLayer.GetInstance(Android.App.Application.Context, ThcompHAL.HttpAccessLayer.Accessor.OkHttp);
+            accessLayer.ResponseCallback(
+                delegate (ThcompHAL.IResponse successResponse)
+                {
+                    Assert.True(successResponse != null);
+                    Assert.True(successResponse.StatusCode == 200);
+                    Assert.True(successResponse.ReasonPhrase != null);
+                    Assert.True(successResponse.GetHeaders(null).Count > 0);
+                    Assert.True(successResponse.GetHeaders("Content-Length").Count > 0);
+                    Assert.True(successResponse.MimeType.StartsWith("text/html"));
+                },
+                delegate (ThcompHAL.IResponse failResponse)
+                {
+                    Assert.True(failResponse != null);
+                    Assert.True(failResponse.StatusCode != 200);
+                }
+            ).Uri("https://www.google.co.jp").Get();
+
+            accessLayer.ResponseCallback((ThcompHAL.HttpAccessLayer.SuccessResponse)
+                delegate (ThcompHAL.IResponse successResponse)
+                {
+                    Assert.True(successResponse != null);
+                    Assert.True(successResponse.StatusCode == 200);
+                    Assert.True(successResponse.ReasonPhrase != null);
+                    Assert.True(successResponse.GetHeaders(null).Count > 0);
+                    Assert.True(successResponse.GetHeaders("Content-Length").Count > 0);
+                    Assert.True(successResponse.MimeType.StartsWith("text/html"));
+                }
+            ).Uri("https://www.google.co.jp").Get();
+
+            accessLayer.ResponseCallback((ThcompHAL.HttpAccessLayer.FailResponse)
+                delegate (ThcompHAL.IResponse failResponse)
+                {
+                    Assert.True(false);
+                }
+            ).Uri("https://www.google.co.jp").Get();
+        }
+
+        [Test]
+        public void TestHttpAccessLayer2()
+        {
+            ThcompHAL.HttpAccessLayer accessLayer = ThcompHAL.HttpAccessLayer.GetInstance(Android.App.Application.Context, ThcompHAL.HttpAccessLayer.Accessor.OkHttp);
+            accessLayer.ResponseCallback(
+                delegate (ThcompHAL.IResponse successResponse)
+                {
+                    Assert.True(successResponse != null);
+                    Assert.True(successResponse.StatusCode == 200);
+                    Assert.True(successResponse.ReasonPhrase != null);
+                    Assert.True(successResponse.GetHeaders(null).Count > 0);
+                    Assert.True(successResponse.GetHeaders("Content-Length").Count > 0);
+                    Assert.True(successResponse.MimeType.StartsWith("text/html"));
+                },
+                delegate (ThcompHAL.IResponse failResponse)
+                {
+                    Assert.True(failResponse != null);
+                    Assert.True(failResponse.StatusCode != 200);
+                }
+            ).Uri("https://www.google.co.jp")
+            .RequestParam(new ThcompHAL.StringRequestParameter("", "nanikaireteokimasyou")).Post();
+
+            accessLayer.ResponseCallback((ThcompHAL.HttpAccessLayer.SuccessResponse)
+                delegate (ThcompHAL.IResponse successResponse)
+                {
+                    Assert.True(false);
+                }
+            ).Uri("https://www.google.co.jp")
+            .RequestParam(new ThcompHAL.StringRequestParameter("", "nanikaireteokimasyou")).Post();
+
+            accessLayer.ResponseCallback((ThcompHAL.HttpAccessLayer.FailResponse)
+                delegate (ThcompHAL.IResponse failResponse)
+                {
+                    Assert.True(failResponse != null);
+                    Assert.True(failResponse.StatusCode != 200);
+                }
+            ).Uri("https://www.google.co.jp")
+            .RequestParam(new ThcompHAL.StringRequestParameter("", "nanikaireteokimasyou")).Post();
+        }
+
+        [Test]
+        public void TestHttpAccessLayer3()
+        {
+            ThcompHAL.HttpAccessLayer accessLayer = ThcompHAL.HttpAccessLayer.GetInstance(Android.App.Application.Context, ThcompHAL.HttpAccessLayer.Accessor.OkHttp);
+            accessLayer.ResponseCallback(
+                delegate (ThcompHAL.IResponse successResponse)
+                {
+                    Assert.True(successResponse != null);
+                    Assert.True(successResponse.StatusCode == 200);
+                    Assert.True(successResponse.ReasonPhrase != null);
+                    Assert.True(successResponse.GetHeaders(null).Count > 0);
+                    Assert.True(successResponse.GetHeaders("Content-Length").Count > 0);
+                    Assert.True(successResponse.MimeType.StartsWith("text/html"));
+                },
+                delegate (ThcompHAL.IResponse failResponse)
+                {
+                    Assert.True(failResponse != null);
+                    Assert.True(failResponse.StatusCode != 200);
+                }
+            ).Uri("https://www.konnna.domainga.naikotowo.negaimasu").Get();
+
+            accessLayer.ResponseCallback((ThcompHAL.HttpAccessLayer.SuccessResponse)
+                delegate (ThcompHAL.IResponse successResponse)
+                {
+                    Assert.True(false);
+                }
+            ).Uri("https://www.konnna.domainga.naikotowo.negaimasu").Get();
+
+            accessLayer.ResponseCallback((ThcompHAL.HttpAccessLayer.FailResponse)
+                delegate (ThcompHAL.IResponse failResponse)
+                {
+                    Assert.True(failResponse != null);
+                    Assert.True(failResponse.StatusCode != 200);
+                }
+            ).Uri("https://www.konnna.domainga.naikotowo.negaimasu").Get();
+        }
+    }
+}
---
+++ XamarinBinding/XamarinBinding/Source/HttpAccessLayer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace JP.CO.Thcomp.Http_abstract_layer
+{
+    public partial class HttpAccessLayer
+    {
+        public delegate void SuccessResponse(IResponse response);
+        public delegate void FailResponse(IResponse response);
+
+        internal class LocalSuccessResponseCallback : Java.Lang.Object, ISuccessResponseCallback
+        {
+            internal SuccessResponse mSuccessResponseDelegator;
+
+            internal LocalSuccessResponseCallback(SuccessResponse delegator)
+            {
+                mSuccessResponseDelegator = delegator ?? throw new ArgumentNullException("delegator == null");
+            }
+
+            public void OnSuccess(IResponse p0)
+            {
+                mSuccessResponseDelegator(p0);
+            }
+        }
+
+        internal class LocalFailResponseCallback : Java.Lang.Object, IFailResponseCallback
+        {
+            internal FailResponse mFailResponseDelegator;
+
+            internal LocalFailResponseCallback(FailResponse delegator)
+            {
+                mFailResponseDelegator = delegator ?? throw new ArgumentNullException("delegator == null");
+            }
+
+            public void OnFail(IResponse p0)
+            {
+                mFailResponseDelegator(p0);
+            }
+        }
+
+        internal class LocalResponseCallback : Java.Lang.Object, IResponseCallback
+        {
+            internal SuccessResponse mSuccessResponseDelegator;
+            internal FailResponse mFailResponseDelegator;
+
+            public LocalResponseCallback(SuccessResponse successResponseDelegator, FailResponse failResponseDelegator)
+            {
+                mSuccessResponseDelegator = successResponseDelegator ?? throw new ArgumentNullException("successResponseDelegator == null");
+                mFailResponseDelegator = failResponseDelegator ?? throw new ArgumentNullException("failResponseDelegator == null");
+            }
+
+            public void OnFail(IResponse p0)
+            {
+                mFailResponseDelegator(p0);
+            }
+
+            public void OnSuccess(IResponse p0)
+            {
+                mSuccessResponseDelegator(p0);
+            }
+        }
+
+        public HttpAccessLayer ResponseCallback(SuccessResponse delegator)
+        {
+            if (delegator == null)
+            {
+                ResponseCallback((IResponseCallback)null);
+            }
+            else
+            {
+                ResponseCallback(new LocalSuccessResponseCallback(delegator));
+            }
+            return this;
+        }
+
+        public HttpAccessLayer ResponseCallback(FailResponse delegator)
+        {
+            if (delegator == null)
+            {
+                ResponseCallback((IResponseCallback)null);
+            }
+            else
+            {
+                ResponseCallback(new LocalFailResponseCallback(delegator));
+            }
+            return this;
+        }
+
+        public HttpAccessLayer ResponseCallback(SuccessResponse successResponseDelegator, FailResponse failResponseDelegator)
+        {
+            if (successResponseDelegator == null && failResponseDelegator == null)
+            {
+                ResponseCallback((IResponseCallback)null);
+            }
+            else if (successResponseDelegator != null && failResponseDelegator == null)
+            {
+                ResponseCallback(new LocalSuccessResponseCallback(successResponseDelegator));
+            }
+            else if (successResponseDelegator == null && failResponseDelegator != null)
+            {
+                ResponseCallback(new LocalFailResponseCallback(failResponseDelegator));
+            }
+            else
+            {
+                ResponseCallback(new LocalResponseCallback(successResponseDelegator, failResponseDelegator));
+            }
+            return this;
+        }
+    }
+}
